90ccd931f1df5f735fea979a40ca290b3703a296
Marco Salsiccia <marco.salsiccia@gmail.com>
2025-12-10 14:06:01 -0800

    Initial Commit

---
+++ ohCraps/ohCrapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ohCrapsApp.swift
+//  ohCraps
+//
+//  Created by Marco Salsiccia on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ohCrapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
