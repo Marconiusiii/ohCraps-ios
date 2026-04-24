991b109e8f753c3262b23f9a6ab7aab4f622f15f
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-04-23 19:31:43 -0700

    My Strategies focus updated, and updated What's New list. Ready for merge to main

--- ohCraps/WhatsNewData.swift
+++ ohCraps/WhatsNewData.swift
@@ -3,6 +3,7 @@ import Foundation
 enum WhatsNewData {
 	static let items = [
 		"Updated VoiceOver focus management when coming back to the Strategies screen after adding or removing a Favorite strategy.",
+		"Same focus fix applied to My Strategies list in the Create Strategy screen.",
 		"Added strategy: Dave's Method"
 	]
 }
