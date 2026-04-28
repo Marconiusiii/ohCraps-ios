486555164c8a79d865e8b9ce6023358cc6f451de
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-04-28 13:25:57 -0700

    Updated style and theming, updated What's New section

--- ohCraps/WhatsNewData.swift
+++ ohCraps/WhatsNewData.swift
@@ -2,8 +2,8 @@ import Foundation
 
 enum WhatsNewData {
 	static let items = [
-		"Added Personal Notes to core and user strategies. These save locally to your device and are not shared, so treat them as a good scratchpad, keep track of winnings, anything you'd like!",
-		"The Personal Notes save immediately as you type them or when you back out of a strategy.",
-		"Interface adjustments in your created strategy screens."
+		"Updated overall style and theming of the app to look more like a Craps table.",
+		"Fixed accessibility of Create Strategy form.",
+		"Fixed color contrast of Search Strategies field."
 	]
 }
